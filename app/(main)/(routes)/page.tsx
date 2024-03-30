302b0a74a52bdd42f55c287a1a090b55685fcd26
treexl <denikquadro@gmail.com>
2024-03-30 16:45:24 +0200

    change

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -2,10 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <UserButton 
-        afterSignOutUrl="/"
-      />
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/"/>
     </div>
   );
 }
