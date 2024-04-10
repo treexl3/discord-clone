2696da2387acc81bbda0ff693022d2d11e59b2dc
treexl3 <denikquadro@gmail.com>
2024-04-10 10:29:41 +0300

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/providers/theme-provider";
+
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -24,6 +26,7 @@ export default function RootLayout({
           font.className,
           "bg-white dark:bg-[#313338]"
         )}>
+          <ModalProvider />
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,11 +1,14 @@
 import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { ModeToggle } from "@/components/mode-toggle";
 import { Separator } from "@/components/ui/separator";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
 import { NavigationAction } from "./navigation-action";
+import { NavigationItem } from "./navigation-item";
 
 export const NavigationSidebar = async () => {
     const profile = await currentProfile();
@@ -32,11 +35,26 @@ export const NavigationSidebar = async () => {
             />
             <ScrollArea className="flex-1 w-full">
                 {servers.map((server) => (
-                    <div key={server.id} clasName="mb-4">
-                        <NavigationItem />
+                    <div key={server.id} className="mb-4">
+                        <NavigationItem 
+                            id={server.id}
+                            name={server.name}
+                            imageUrl={server.imageUrl}
+                        />
                     </div>
                 ))}
             </ScrollArea>
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+                <ModeToggle />
+                <UserButton
+                    afterSignOutUrl="/"
+                    appearance={{
+                        elements: {
+                            avatarBox: "h-[48px] w-[48px]"
+                        }
+                    }}
+                />
+            </div>
         </div>
     )
 }
